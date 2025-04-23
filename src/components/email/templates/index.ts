873160f235155b6abe805f52cda593b5fae98da3
banderson651 <banderson651@insite.4cd.edu>
2025-04-23 11:41:19 +0530

    Revert "Test commit message"
    This reverts commit 0cadde6c99e4214a0a5728333a3b7b57f1f91ccc.

--- src/components/email/templates/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './PropertyListingTemplate';
-export * from './OpenHouseInvitationTemplate';
-export * from './ClientFollowUpTemplate';
-export * from './MarketUpdateTemplate';
-export * from './TransactionCompleteTemplate';
-export * from './types';
